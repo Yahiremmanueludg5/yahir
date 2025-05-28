962251e2c7e2dcd2fef8286959e87966845a18f9
JulioPalaEducacion <julio.palacios2816@alumnos.udg.mx>
2025-05-28 13:51:05 -0600

    sql

--- static/sql/antojo.sql
+++ static/sql/antojo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 28-03-2025 a las 18:38:05
+-- Tiempo de generación: 28-05-2025 a las 19:50:31
 -- Versión del servidor: 8.0.41
 -- Versión de PHP: 7.4.9
 
@@ -85,6 +85,30 @@ CREATE TABLE `partes_aplicacion` (
   `descripcion` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int NOT NULL,
+  `nombre` varchar(250) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `clave` varchar(250) NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `fechareg` datetime NOT NULL,
+  `perfil` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'S'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nombre`, `correo`, `clave`, `telefono`, `fechareg`, `perfil`) VALUES
+(1, 'pancho', 'yahir.alvarez2830@alumnos.udg.mx', 'scrypt:32768:8:1$vaaNa28gqVRAOCZw$d33afc51b8f0c46bfa1f6a0796338dce5a04c4fe0df5ac3bb8dd208c1f8ec40b81d7d4f512fd866942f4cb76fc0ba2c06bec8e2fb35dde229321736f9f0fe7e8', '3322344813', '2025-05-28 13:25:02', 'S'),
+(2, 'ale', 'ale235@alumnos.udg.mx', 'scrypt:32768:8:1$i653HwoxGOrUZt4L$322f70967f30838b827c7773f9c1306063a32bf59923730eccff21eafa5d4adb060a6b82c500a0f50a2d7fcf699c9f4011623c499defc5f5eb13fc1464110341', '3325147891', '2025-05-28 13:27:23', 'S');
+
 --
 -- Índices para tablas volcadas
 --
@@ -114,6 +138,13 @@ ALTER TABLE `partes_aplicacion`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -140,6 +171,12 @@ ALTER TABLE `fotos comida`
 --
 ALTER TABLE `partes_aplicacion`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
