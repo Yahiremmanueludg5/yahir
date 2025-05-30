c268edac1bcc4d471902388b14b4b614217a3af4
RayoMcqueen10 <pedro.cernas1877@alumnos.udg.mx>
2025-05-30 12:57:50 -0600

    final

--- antojo.py
+++ antojo.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, url_for,request, redirect,flash
 from config import config
-from models.entities import User
-from models.entities.ModelUser import ModelUser
+from models.entities.User import User
+from models.ModelUser import ModelUser
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash
 from datetime import datetime
-from flask_login import login_user, LoginManager, logout_user
+from flask_login import LoginManager, logout_user, login_user, current_user
 from flask_mysqldb import MySQL
-
-
 
 antojo = Flask(__name__)
 db      = MySQL(antojo)
@@ -16,7 +14,7 @@ adminSesion = LoginManager(antojo)
 
 @adminSesion.user_loader
 def load_user(id):
-        return ModelUser.get_by_id(db,id)
+    return ModelUser.get_by_id(db,id)
 
 @antojo.route('/')
 def home():
@@ -77,7 +75,11 @@ def iUsuario():
     telefono = request.form['telefono']
     fechareg = datetime.now()
     regUsuario = db.connection.cursor()
-    regUsuario.execute("INSERT INTO usuario(nombre,correo,clave,telefono,fechareg) VALUES (%s,%s,%s,%s,%s)",nombre,correo,claveCifrada,telefono,fechareg)
+    regUsuario.execute(
+    "INSERT INTO usuario(nombre,correo,clave,telefono,fechareg) VALUES (%s,%s,%s,%s,%s)", 
+    (nombre, correo, claveCifrada, telefono, fechareg)
+)
+
     db.connection.commit()
     regUsuario.close()
     return redirect(url_for('sUsuario'))
@@ -101,7 +103,7 @@ def uUsuario(id):
 @antojo.route('/dUsuario/<int:id>', methods=['GET','POST'])
 def dUsuario(id):
     delUsuario = db.connection.cursor()
-    delUsuario.execute("DELATE FROM usuario WHERE id=%s",(id))
+    delUsuario.execute("DELETE FROM usuario WHERE id=%s",(id,))
     db.connection.commit()
     delUsuario.close()
     flash('Usuario Eliminado')
